5b234c9f109816d88792eb8af26cadc9e328d3b2
Wesley-waka <wesleywaka77@gmail.com>
2024-06-14 12:35:56 +0300

    Implement Provider for redux

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,26 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+import { ApolloProvider } from '@apollo/client';
+import client from './services/api-client.ts';
+import './index.css';
+import store from './redux/store/store.ts';
+import { Provider } from 'react-redux';
+// const theme = createTheme({
+//   palette: {
+//     primary: {
+//       main: '#5acccc',
+//     },
+//   },
+// });
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <>
+    <ApolloProvider client={client}>
+    <Provider store={store}>
+    {/* <ThemeProvider theme={theme}> */}
+      <App />
+    {/* </ThemeProvider> */}
+    </Provider>
+  </ApolloProvider>
+  </>,
 )
