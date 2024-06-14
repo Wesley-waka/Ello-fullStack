35f36cb30526feb99cce9c0a67665e090aabd894
Wesley-waka <wesleywaka77@gmail.com>
2024-06-14 12:34:28 +0300

    Implement store for app state

---
+++ src/redux/store/store.ts
@@ -0,0 +1,7 @@
+// store.ts
+import { configureStore } from '@reduxjs/toolkit';
+import rootReducer from '../reducers';
+
+const store = configureStore({reducer: rootReducer});
+
+export default store;
